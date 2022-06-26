7cff4a54a9093f2d4aeee62d9f1bb35a67b9575f
LukaszWojsz <lukasz.wojsz94@gmail.com>
2022-06-26 12:22:58 +0200

    products page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import { CssSyntaxError } from "postcss";
 import style from "./css/style.scss"
 
 $(document).ready(function(){
     $("#checkoutBtn").click(function(){
       $("#about").scrollTo();
     });
-  });
+  }); 
+
+// if(window.location !== "#home"){
+//   document.getElementById('halfBar2').style.backgroundColor = "red";
+// }else{
+//   document.getElementById('halfBar2').style.backgroundColor = "transparent";
+// };
+
