517a6673cc3fe363bda82be20532e9cc41befb8c
LukaszWojsz <lukasz.wojsz94@gmail.com>
2022-07-28 19:42:23 +0200

    catalog 28.07 + navbar effects

--- src/catalog.js
+++ src/catalog.js
@@ -2,43 +2,74 @@ import { CssSyntaxError } from "postcss";
 import style from "./css/style.scss";
 import "./rwdNav";
 
+let clicked = false;
 
 let glamourChair = document.getElementById("glamourChair");
 let upholsteryChair = document.getElementById("upholsteryChair");
 let woodenChair = document.getElementById("woodenChair");
 let loftTable = document.getElementById("loftTable");
+let woodenTable = document.getElementById("woodenTable");
+let coffeeTable = document.getElementById("coffeeTable");
 
 let chairsGlamourContainer = document.getElementById("chairsGlamour");
 let chairsUpholsteryContainer = document.getElementById("chairsUpholstery");
 let chairsWoodenContainer = document.getElementById("chairsWooden");
 let tablesLoftContainer = document.getElementById("tablesLoft");
+let tablesWoodenContainer = document.getElementById("tablesWooden");
+let tableCoffeeContainer = document.getElementById("tableCoffee");
 
-
-upholsteryChair.addEventListener('click', ()=>{
-    chairsUpholsteryContainer.style.display = "flex";
-    chairsGlamourContainer.style.display = "none";
-    chairsWoodenContainer.style.display = "none"
-    tablesLoftContainer.style.display = "none";
-    
+$("li.active").click(function () {
+  $("li.active").css("transform", "scale(1)");
+  $(this).css("transform", "scale(1.3)");
+  $(this).css("transition", "0.5s");
 });
-glamourChair.addEventListener('click',()=>{
-    chairsGlamourContainer.style.display = "flex";
-    chairsUpholsteryContainer.style.display = "none";
-    chairsWoodenContainer.style.display = "none";
-    tablesLoftContainer.style.display = "none";
-});
-woodenChair.addEventListener('click',()=>{
-    chairsWoodenContainer.style.display = "flex";
-    chairsGlamourContainer.style.display = "none";
-    chairsUpholsteryContainer.style.display = "none";
-    tablesLoftContainer.style.display = "none";
-});
-loftTable.addEventListener('click', ()=>{
-    tablesLoftContainer.style.display = "flex";
-    chairsWoodenContainer.style.display = "none";
-    chairsGlamourContainer.style.display = "none";
-    chairsUpholsteryContainer.style.display = "none";
-    
-})
 
 
+upholsteryChair.addEventListener("click", () => {
+  chairsUpholsteryContainer.style.display = "flex";
+  chairsGlamourContainer.style.display = "none";
+  chairsWoodenContainer.style.display = "none";
+  tablesLoftContainer.style.display = "none";
+  tablesWoodenContainer.style.display = "none";
+  tableCoffeeContainer.style.display = "none";
+});
+glamourChair.addEventListener("click", () => {
+  chairsGlamourContainer.style.display = "flex";
+  chairsUpholsteryContainer.style.display = "none";
+  chairsWoodenContainer.style.display = "none";
+  tablesLoftContainer.style.display = "none";
+  tablesWoodenContainer.style.display = "none";
+  tableCoffeeContainer.style.display = "none";
+});
+woodenChair.addEventListener("click", () => {
+  chairsWoodenContainer.style.display = "flex";
+  chairsGlamourContainer.style.display = "none";
+  chairsUpholsteryContainer.style.display = "none";
+  tablesLoftContainer.style.display = "none";
+  tablesWoodenContainer.style.display = "none";
+  tableCoffeeContainer.style.display = "none";
+});
+loftTable.addEventListener("click", () => {
+  tablesLoftContainer.style.display = "flex";
+  chairsWoodenContainer.style.display = "none";
+  chairsGlamourContainer.style.display = "none";
+  chairsUpholsteryContainer.style.display = "none";
+  tablesWoodenContainer.style.display = "none";
+  tableCoffeeContainer.style.display = "none";
+});
+woodenTable.addEventListener("click", () => {
+  tablesWoodenContainer.style.display = "flex";
+  tablesLoftContainer.style.display = "none";
+  chairsWoodenContainer.style.display = "none";
+  chairsGlamourContainer.style.display = "none";
+  chairsUpholsteryContainer.style.display = "none";
+  tableCoffeeContainer.style.display = "none";
+});
+coffeeTable.addEventListener("click", () => {
+  tableCoffeeContainer.style.display = "flex";
+  tablesWoodenContainer.style.display = "none";
+  tablesLoftContainer.style.display = "none";
+  chairsWoodenContainer.style.display = "none";
+  chairsGlamourContainer.style.display = "none";
+  chairsUpholsteryContainer.style.display = "none";
+});
